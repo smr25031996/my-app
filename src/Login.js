84f5a69dd5ecd7713072743313beeadbe2273cc6
Shubham <rajputhsubham615@gmail.com>
2024-11-18 22:25:39 +0530

    revised the how JSX is useful productivity

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+function Login() {
+  return (
+    <form>
+      <h1>Log In Page</h1>
+      <input type="text" placeholder="Name" />
+      <br /> <br />
+      <input type="password" placeholder="Password" />
+      <br /> <br />
+      <input type="submit" nvalue="log" />
+      <br /> <br />
+    </form>
+  );
+}
+
+export default Login;
