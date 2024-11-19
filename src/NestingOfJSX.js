04e00c47b54c90b62492a51fa96375ab4cdb784a
Shubham <rajputhsubham615@gmail.com>
2024-11-19 22:14:00 +0530

    learnt nesting of jsx element

---
+++ src/NestingOfJSX.js
@@ -0,0 +1,16 @@
+function NestingOfJSX() {
+  return (
+    <div>
+      <h3>React JS :</h3>
+      <img
+        src="./images/logo192.png"
+        width="120"
+        height="120"
+        alt="imageLogo"
+      />
+      <p> React is a JavaScript library for creating User Interfaces.</p>
+    </div>
+  );
+}
+
+export default NestingOfJSX;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Login from "./Login";
+import NestingOfJSX from "./NestingOfJSX";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  {
-    /* <App /> */
-  },
-  <Login />
+  <div>
+    <App />,
+    <Login />,
+    <NestingOfJSX />
+  </div>
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
